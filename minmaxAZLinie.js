c1047e7245cdf6923a64ccd607b749e363caa0d9
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2022-12-15 09:46:49 +0100

    Update minmaxAZLinie.js

--- minmaxAZLinie.js
+++ minmaxAZLinie.js
@@ -1,6 +1,6 @@
 //Tabela ze znakami UTF8
 var UTF8 = {A:65,B:66,C:67,D:68,E:69,F:70,G:71,H:72,I:73,J:74,K:75,L:76,M:77,N:78,O:79,P:80,Q:81,R:82,S:83,T:84,U:85,V:86,W:87,X:88,Y:89,Z:90}
-var input = "ADSBRWS\nHDFSWETS\nAGDFSDFS" //Ustalone z góry dane wejściowe \n to odpowiednik ENTER
+var input = "ADSBRWS\nHDFSWETS\nAGDFSDFS\n^fs8" //Ustalone z góry dane wejściowe \n to odpowiednik ENTER
 var segregated = []; //Posegreguj dane zgodnie z liniami
 var result = ""; //Chwilowe przechowanie danych z pętli for
 for(let i=0;i<input.length;i++){
@@ -9,14 +9,14 @@ for(let i=0;i<input.length;i++){
         result=result+data;
     } else {
         segregated[segregated.length]=result;
-        result="";
+        result=""; //Wyczyść result dla następnej iteracji
     };
     if(i==input.length-1){
         segregated[segregated.length]=result;
-        result=undefined;
+        result=undefined; //Wyczyść result na koniec pętli (nie wymagane)
     };
 };
-segregated.forEach(sector=>{
+segregated.forEach(sector=>{ //Dla każdego rekordu z tabeli segregated || iteracji tabeli segregated
     let min=100; //min musi być większa niż tabela UTF-8 aby porównać poprawnie znaki
     let max=0; //max musi być niższa niż tabela UTF-8 aby porównać poprawnie znaki
     for(let i=0;i<sector.length;i++){
