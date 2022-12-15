afcbe8c0dff72e1dbfa2d1c33b33795d63ebe083
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2022-12-15 09:44:26 +0100

    Update najwiekszyznakAZ.js

--- najwiekszyznakAZ.js
+++ najwiekszyznakAZ.js
@@ -1,21 +1,37 @@
-//Polecenie
-/*
-    znajdź najwyższy znak z ciągu znaków od A-Z wielkością ASCII(UTF-8)
-*/
-
-//Tabela z wartościami od A-Z od 65-90 kolejno
+//Tabela ze znakami UTF8
 var UTF8 = {A:65,B:66,C:67,D:68,E:69,F:70,G:71,H:72,I:73,J:74,K:75,L:76,M:77,N:78,O:79,P:80,Q:81,R:82,S:83,T:84,U:85,V:86,W:87,X:88,Y:89,Z:90}
-var input = "AGDVXHFS" //Ustalone z góry dane wejściowe
-//Pętla for(dla), przyjmuje 3 warunki, wartość wejściową,warunek kontynuacji pętli, iteracja wartości wejściowej || i++,i--
-var max = 0;
+var input = "ADSBRWS\nHDFSWETS\nAGDFSDFS\n^fs8" //Ustalone z góry dane wejściowe \n to odpowiednik ENTER
+var segregated = []; //Posegreguj dane zgodnie z liniami
+var result = ""; //Chwilowe przechowanie danych z pętli for
 for(let i=0;i<input.length;i++){
-    let string = input.substring(i,i+1); //Weź znak na pozycji od i do i+1 0a1
-    if(!string||!UTF8[string])continue; //Jeżeli nie ma znaku w naszej tabeli, pomiń dalszy kod
-    if(max<UTF8[string])max=UTF8[string]; //Jeżeli zmienna max jest mniejsza od wartości w tabeli, nadpisz max
+    let data = input.substring(i,i+1);
+    if(data!='\n'){
+        result=result+data;
+    } else {
+        segregated[segregated.length]=result;
+        result=""; //Wyczyść result dla następnej iteracji
+    };
+    if(i==input.length-1){
+        segregated[segregated.length]=result;
+        result=undefined; //Wyczyść result na koniec pętli (nie wymagane)
+    };
 };
-if(max==0){ //Jeżeli nie ma znaku w naszej tabeli, zwróć wiadomość
-    console.log('Złe wprowadzone dane!');
-} else { //Inaczej
-    console.log('Najwyższa liczba w ciągu znaków to: '+max+' UTF-8: '+String.fromCharCode(max));
-    console.log(`Najwyższa liczba w ciągu znaków to: ${max} UTF-8: ${String.fromCharCode(max)}`); // Ta sama wiadomość inną metodą
-};
+segregated.forEach(sector=>{ //Dla każdego rekordu z tabeli segregated || iteracji tabeli segregated
+    let min=100; //min musi być większa niż tabela UTF-8 aby porównać poprawnie znaki
+    let max=0; //max musi być niższa niż tabela UTF-8 aby porównać poprawnie znaki
+    for(let i=0;i<sector.length;i++){
+        let data = sector.substring(i,i+1); //0sector1
+        if(!data||!UTF8[data])continue; //Jeżeli nie ma znaku w UTF8 pomiń tą iteracje
+        if(min>UTF8[data])min=UTF8[data]; // Jeżeli min większe od UTF8 to nadpisz min
+        if(max<UTF8[data])max=UTF8[data]; // Jeżeli max mniejsze od UTF8 to nadpisz max
+    };
+    if(min!=100){ // Jeżeli min nie jest 100 to z góry wiadomo że max nie jest 0
+        console.log('W ciągu znaków: '+sector+'\nNajwiększym znakiem jest: '+max+' UTF-8: '+String.fromCharCode(max)+'\nNajmniejszym znakiem jest: '+min+' UTF-8: '+String.fromCharCode(min));
+        //Ta sama wiadomość innym sposobem
+        console.log(`W ciągu znaków: ${sector}\nNajwiększym znakiem jest: ${max} UTF-8: ${String.fromCharCode(max)}\nNajmniejszym znakiem jest: ${min} UTF-8: ${String.fromCharCode(min)}`);
+    } else {
+        console.log('W ciągu znaków: '+sector+'\nNie znaleziono żadnych poprawnych znaków!');
+        //Ta sama wiadomość innym sposobem
+        console.log(`W ciągu znaków: ${sector}\nNie znaleziono żadnych poprawnych znaków!`)
+    };
+});
