69749ed045f1a086b3e41433607db91b5219bae7
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2022-12-15 12:09:34 +0100

    Update liczenieliter.js

--- liczenieliter.js
+++ liczenieliter.js
@@ -1,3 +1,13 @@
+//Polecenie
+/* 
+    Znajdź największy znak w tabeli n-elementowej, gdzie wszystkie elementy do znaki a-z i A-Z
+    a-z mające niższą wartość niż A-Z, gdzie ciąg a-z i A-Z jest nieprzerwana
+    Wypisz wartość znaku i jego literę
+
+    Dodatkowo:
+    Policz występowanie każdego znaku w ciągu znaków
+*/
+
 //tabela ze znakami a-z i A-Z z wartościami idącymi od 1-52
 var chars = {a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13,n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26,A:27,B:28,C:29,D:30,E:31,F:32,G:33,H:34,I:35,J:36,K:37,L:38,M:39,N:40,O:41,P:42,Q:43,R:44,S:45,T:46,U:47,V:48,W:49,X:50,Y:51,Z:52}
 //tabela ze znakami a-z i A-Z na pozycjach od 0-51
