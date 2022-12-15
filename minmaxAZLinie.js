44a1da8ea777906871bddb055c03d5e00e32fdf2
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2022-12-15 12:10:10 +0100

    Update minmaxAZLinie.js

--- minmaxAZLinie.js
+++ minmaxAZLinie.js
@@ -1,3 +1,8 @@
+// //Polecenie
+// /*
+//     W każdej lini znajdź największy i najmniejszy znak A-Z
+// */
+
 //Tabela ze znakami UTF8
 var UTF8 = {A:65,B:66,C:67,D:68,E:69,F:70,G:71,H:72,I:73,J:74,K:75,L:76,M:77,N:78,O:79,P:80,Q:81,R:82,S:83,T:84,U:85,V:86,W:87,X:88,Y:89,Z:90}
 var input = "ADSBRWS\nHDFSWETS\nAGDFSDFS\n^fs8" //Ustalone z góry dane wejściowe \n to odpowiednik ENTER
